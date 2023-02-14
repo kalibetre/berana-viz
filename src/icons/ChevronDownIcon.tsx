946911eaafe62fa96c79ffeaa0421bd511481778
kalibetre <kalbetre@icloud.com>
2023-02-14 23:16:22 +0300

    add icons

---
+++ src/icons/ChevronDownIcon.tsx
@@ -0,0 +1,11 @@
+const ChevronDownIcon = () => {
+    return (
+        <img
+            style={{ width: '100%', height: '100%' }}
+            src="/images/chevron-arrow-icon.svg"
+            alt="Document Icon"
+        />
+    );
+};
+
+export default ChevronDownIcon;
---
+++ src/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as ChevronDownIcon } from './ChevronDownIcon';
+export { default as DocumentIcon } from './DocumentIcon';
+export { default as GitHubIcon } from './GitHubIcon';
+export { default as UserAvatarIcon } from './UserAvatarIcon';
