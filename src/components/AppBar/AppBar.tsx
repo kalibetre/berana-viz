c7485fbbfe7c201e74c3bb9b88465df4fff18b10
kalibetre <kalbetre@icloud.com>
2023-02-15 00:53:29 +0300

    fix layout issue for longer content in main

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -3,33 +3,35 @@ import styles from './AppBar.module.css';
 
 const AppBar = () => {
     return (
-        <header className={styles.header}>
-            <div className={styles.logoContainer}>
-                <img
-                    className={styles.logo}
-                    src="/images/logo.png"
-                    alt="BeranaVis Logo"
-                />
-                <div className={styles.logoTitles}>
-                    <h1 className={styles.logoTitle}>BeranaVis</h1>
-                    <h2 className={styles.logoSubTitle}>
-                        Data Structures and Algorithms Visualizer
-                    </h2>
+        <header>
+            <div className={styles.content}>
+                <div className={styles.logoContainer}>
+                    <img
+                        className={styles.logo}
+                        src="/images/logo.png"
+                        alt="BeranaVis Logo"
+                    />
+                    <div className={styles.logoTitles}>
+                        <h1 className={styles.logoTitle}>BeranaVis</h1>
+                        <h2 className={styles.logoSubTitle}>
+                            Data Structures and Algorithms Visualizer
+                        </h2>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.avatarContainer}>
-                <span className={styles.icon}>
-                    <UserAvatarIcon />
-                </span>
-                <span className={styles.icon}>
-                    <a
-                        href="https://github.com/kalibetre/berana-vis"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <GitHubIcon />
-                    </a>
-                </span>
+                <div className={styles.avatarContainer}>
+                    <span className={styles.icon}>
+                        <UserAvatarIcon />
+                    </span>
+                    <span className={styles.icon}>
+                        <a
+                            href="https://github.com/kalibetre/berana-vis"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <GitHubIcon />
+                        </a>
+                    </span>
+                </div>
             </div>
         </header>
     );
