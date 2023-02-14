9832f4c8a2e046ccf5ddeae61bb7dbc939ffab84
kalibetre <kalbetre@icloud.com>
2023-02-15 00:53:52 +0300

    add chevron up icon to index export

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as ChevronDownIcon } from './ChevronDownIcon';
+export { default as ChevronUpIcon } from './ChevronUpIcon';
 export { default as DocumentIcon } from './DocumentIcon';
 export { default as GitHubIcon } from './GitHubIcon';
 export { default as UserAvatarIcon } from './UserAvatarIcon';
