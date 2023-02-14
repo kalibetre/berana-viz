2eea6be3918c9ed2575282948f36d914c8352acf
kalibetre <kalbetre@icloud.com>
2023-02-14 23:32:04 +0300

    add collapsible toolbox component

---
+++ src/components/ToolBox/ToolBox.tsx
@@ -0,0 +1,43 @@
+import { ReactNode, useState } from 'react';
+import { ChevronDownIcon, ChevronUpIcon } from '../../icons';
+import styles from './ToolBox.module.css';
+
+interface ToolBoxProps {
+    title: string;
+    icon?: ReactNode;
+    expanded?: boolean;
+    children: ReactNode;
+}
+
+const ToolBox = (props: ToolBoxProps) => {
+    const [contentOpen, setContentOpen] = useState(props.expanded);
+
+    const handleToggleClick = () => {
+        setContentOpen((prev) => !prev);
+    };
+
+    return (
+        <section className={styles.container}>
+            <header className={styles.header}>
+                <div className={styles.headerTitle}>
+                    {props.icon && (
+                        <span className={styles.icon}>{props.icon}</span>
+                    )}
+                    {props.title}
+                </div>
+                <span className={styles.arrowDown} onClick={handleToggleClick}>
+                    {contentOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+                </span>
+            </header>
+            {contentOpen && (
+                <div className={styles.content}>{props.children}</div>
+            )}
+        </section>
+    );
+};
+
+ToolBox.defaultProps = {
+    expanded: true,
+};
+
+export default ToolBox;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as AppBar } from './AppBar/AppBar';
+export { default as ToolBox } from './ToolBox/ToolBox';
