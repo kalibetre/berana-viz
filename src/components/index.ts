fde9e683e1dc31f6b564b40bf5152c2585d489a2
kalibetre <kalbetre@icloud.com>
2023-02-15 14:45:57 +0300

    add document item and button item components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as AppBar } from './AppBar/AppBar';
+export { default as ButtonItem } from './ToolBox/ButtonItem/ButtonItem';
+export { default as DocumentItem } from './ToolBox/DocumentItem/DocumentItem';
 export { default as ToolBox } from './ToolBox/ToolBox';
 export { default as ToolBoxGroup } from './ToolBox/ToolBoxGroup/ToolBoxGroup';
-export { default as ToolBoxItem } from './ToolBox/ToolBoxItem/ToolBoxItem';
