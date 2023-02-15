9b7d20559c0f7f077e39f3e87dca27fd39ab2077
kalibetre <kalbetre@icloud.com>
2023-02-15 15:05:50 +0300

    add Export Icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,6 +3,7 @@ export { default as ChevronUpIcon } from './ChevronUpIcon';
 export { default as CloudDataIcon } from './CloudDataIcon';
 export { default as DeleteFileIcon } from './DeleteFileIcon';
 export { default as DocumentIcon } from './DocumentIcon';
+export { default as ExportIcon } from './ExportIcon';
 export { default as GitHubIcon } from './GitHubIcon';
 export { default as NewFileIcon } from './NewFileIcon';
 export { default as SaveFileIcon } from './SaveFileIcon';
