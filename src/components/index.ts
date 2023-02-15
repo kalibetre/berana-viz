ae43a3a4407c24889bac60b38ab1bdbff66bb7a7
kalibetre <kalbetre@icloud.com>
2023-02-15 19:22:52 +0300

    add SideToolBox component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as AppBar } from './AppBar/AppBar';
 export { default as ButtonItem } from './ToolBox/ButtonItem/ButtonItem';
 export { default as DocumentItem } from './ToolBox/DocumentItem/DocumentItem';
+export { default as SideToolBox } from './ToolBox/SideToolBox';
 export { default as ToolBox } from './ToolBox/ToolBox';
 export { default as ToolBoxGroup } from './ToolBox/ToolBoxGroup/ToolBoxGroup';
