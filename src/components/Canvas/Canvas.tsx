a08e24f16fb4448cdb5c3979f861b446090221a5
kalibetre <kalbetre@icloud.com>
2023-02-16 20:58:32 +0300

    add a Canvas Component

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,17 +1,11 @@
-import { useEffect, useRef } from 'react';
+import { BarChart } from '..';
 import styles from './Canvas.module.css';
 
 const Canvas = () => {
-    const canvasRef = useRef<SVGSVGElement>(null);
-
-    useEffect(() => {
-        console.log(canvasRef?.current?.getBoundingClientRect());
-    }, []);
-
     return (
-        <svg ref={canvasRef} className={styles.canvas}>
-            Canvas
-        </svg>
+        <div className={styles.container}>
+            <BarChart />
+        </div>
     );
 };
 
