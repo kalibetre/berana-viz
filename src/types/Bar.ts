dcf52eed2bcb08d5ca028ed567858f7b5ce7a2de
kalibetre <kalbetre@icloud.com>
2023-02-16 20:54:42 +0300

    add utility types and constants

---
+++ src/types/Bar.ts
@@ -0,0 +1,10 @@
+import Size from './size';
+
+interface Bar extends Size {
+    value: number;
+    x: number;
+    y: number;
+    color: any;
+}
+
+export default Bar;
---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export type { default as Bar } from './Bar';
+export type { default as ColorRange } from './ColorRange';
+export type { default as Size } from './size';
