4dcd19bac4443ab090979a7b08f99324d8b540e0
kalibetre <kalbetre@icloud.com>
2023-02-17 15:43:15 +0300

    use ModalProvider

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,28 @@
 import { AppBar, Canvas, DocumentSideBar, VisualizationSideBar } from '..';
+import { ModalProvider } from '../Providers';
 import styles from './App.module.css';
 
 const App = () => {
     return (
-        <div className={styles.mainContainer}>
-            <AppBar />
-            <main className={styles.main}>
-                <Canvas />
-                <div className={styles.leftToolBox}>
-                    <DocumentSideBar />
-                </div>
-                <div className={styles.rightToolBox}>
-                    <VisualizationSideBar />
-                </div>
-            </main>
-            <footer>
-                <span className={styles.footer}>
-                    Copy right &#169; Kalkidan Betre
-                </span>
-            </footer>
-        </div>
+        <ModalProvider>
+            <div className={styles.mainContainer}>
+                <AppBar />
+                <main className={styles.main}>
+                    <Canvas />
+                    <div className={styles.leftToolBox}>
+                        <DocumentSideBar />
+                    </div>
+                    <div className={styles.rightToolBox}>
+                        <VisualizationSideBar />
+                    </div>
+                </main>
+                <footer>
+                    <span className={styles.footer}>
+                        Copy right &#169; Kalkidan Betre
+                    </span>
+                </footer>
+            </div>
+        </ModalProvider>
     );
 };
 
