5f1cc020db921a250aba804e4da510b1590377d3
kalibetre <kalbetre@icloud.com>
2023-02-17 15:43:04 +0300

    add components to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 export { default as App } from './App/App';
 export { default as AppBar } from './AppBar/AppBar';
 export { default as Canvas } from './Canvas/Canvas';
+export { default as DocumentActions } from './DocumentsSideBar/DocumentActions/DocumentActions';
 export { default as DocumentSideBar } from './DocumentsSideBar/DocumentsSideBar';
+export { default as NodeActions } from './DocumentsSideBar/NodeActions/NodeActions';
 export { default as Modal } from './Modal/Modal';
 export { default as SideBar } from './SideBar/SideBar';
 export { default as ButtonItem } from './ToolBox/ButtonItem/ButtonItem';
@@ -9,4 +11,5 @@ export { default as DocumentItem } from './ToolBox/DocumentItem/DocumentItem';
 export { default as ToolBox } from './ToolBox/ToolBox';
 export { default as ToolBoxGroup } from './ToolBox/ToolBoxGroup/ToolBoxGroup';
 export { default as BarChart } from './Visualization/BarkChart/BarChart';
+export { default as AddNodeModal } from './Visualization/Modals/AddNodeModal';
 export { default as VisualizationSideBar } from './VisualizationsSideBar/VisualizationSideBar';
