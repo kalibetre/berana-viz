98103d5c067b80db2e8a24db7edf570eae5fe575
kalibetre <kalbetre@icloud.com>
2023-02-18 17:43:07 +0300

    implement node selection

--- src/components/Visualization/BarkChart/BarChart.tsx
+++ src/components/Visualization/BarkChart/BarChart.tsx
@@ -1,19 +1,22 @@
 import chroma from 'chroma-js';
 import * as d3 from 'd3';
 import { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useWindowResize } from '../../../hooks';
+import { Node, nodeSelected } from '../../../store/slices/arraySlice';
+import { AppDispatch, arraySelectors, RootState } from '../../../store/store';
 import { Bar, Size } from '../../../types';
 import { CHART_COLOR_RANGE } from '../../../utils/constants';
 import styles from './BarChart.module.css';
-
-import { useSelector } from 'react-redux';
-import { Node } from '../../../store/slices/arraySlice';
-import { arraySelectors } from '../../../store/store';
-
 const NODE_RECT: Size = { width: 40, height: 25 };
 
 const BarChart = () => {
     const nodes: Node[] = useSelector(arraySelectors.selectAll);
+    const selectedNodeId = useSelector<RootState>(
+        (state) => state.array.selectedId
+    );
+
+    const dispatch: AppDispatch = useDispatch<AppDispatch>();
 
     const [bars, setBars] = useState<Bar[]>([]);
     const canvasRef = useRef<SVGSVGElement>(null);
@@ -21,7 +24,6 @@ const BarChart = () => {
 
     useEffect(() => {
         if (nodes.length === 0) return;
-        const data = nodes.map((node) => node.value);
         const rect = canvasRef.current?.getBoundingClientRect();
         const midPoint = {
             x: rect ? rect.width / 2 : 0,
@@ -32,42 +34,51 @@ const BarChart = () => {
         const colors = chroma.scale([low, mid, high]).mode('hsl');
         const xScale = d3
             .scaleBand()
-            .range([0, data.length * NODE_RECT.width])
+            .range([0, nodes.length * NODE_RECT.width])
             .padding(0.1);
         const yScale = d3
             .scaleLinear()
             .range([(rect ? rect?.height : 0) - 50, 50]);
         const colorScale = d3.scaleLinear();
 
-        xScale.domain(data.map((value, i) => `${i}-${value.toString()}`));
-        yScale.domain([0, Math.max(...data)]);
-        colorScale.domain([Math.min(...data), Math.max(...data)]);
+        xScale.domain(nodes.map((node) => node.id));
 
-        const stPtX = midPoint.x - (data.length * 1.05 * NODE_RECT.width) / 2;
-        const newBars = data.map((d: number, i: number) => {
-            let xPt = stPtX + (xScale(`${i}-${d.toString()}`) || 0);
-            let yPt = yScale(d);
+        const values = nodes.map((node) => node.value);
+        yScale.domain([0, Math.max(...values)]);
+        colorScale.domain([Math.min(...values), Math.max(...values)]);
+
+        const stPtX = midPoint.x - (nodes.length * 1.05 * NODE_RECT.width) / 2;
+        const newBars = nodes.map((node: Node, i: number) => {
+            let xPt = stPtX + (xScale(node.id) || 0);
+            let yPt = yScale(node.value);
 
             return {
-                value: d,
+                id: node.id,
+                value: node.value,
                 x: xPt,
                 y: yPt,
                 width: xScale.bandwidth(),
                 height: yScale(0) - yPt,
-                color: colors(colorScale(d)),
+                color: colors(colorScale(node.value)),
             };
         });
 
-        console.log(newBars);
         setBars(newBars);
     }, [nodes, size]);
+
+    const handleNodeClick = (id: string) => {
+        dispatch(nodeSelected(id));
+    };
 
     return (
         <svg ref={canvasRef} className={styles.canvas} scale={0.5}>
             {bars.map((bar, i) => (
-                <g key={`g-${i}`}>
+                <g key={bar.id} onClick={() => handleNodeClick(bar.id)}>
                     <rect
-                        key={`rect-bs-${i}`}
+                        className={
+                            bar.id === selectedNodeId ? styles.activeNode : ''
+                        }
+                        key={`rect-bs-${bar.id}`}
                         x={bar.x}
                         y={bar.y + bar.height - NODE_RECT.height}
                         width={bar.width}
@@ -78,7 +89,7 @@ const BarChart = () => {
                     />
                     <rect
                         className={styles.bar}
-                        key={`rect-${i}`}
+                        key={`rect-${bar.id}`}
                         x={bar.x}
                         y={bar.y - 3 - NODE_RECT.height}
                         width={bar.width}
@@ -86,7 +97,8 @@ const BarChart = () => {
                         fill={bar.color}
                     />
                     <text
-                        key={`text-${i}`}
+                        className={styles.nodeText}
+                        key={`text-${bar.id}`}
                         x={bar.x + bar.width / 2}
                         y={
                             bar.y +
