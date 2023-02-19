4560c65d10e29134c7ed663ed84971a1f347483f
kalibetre <kalbetre@icloud.com>
2023-02-19 22:41:15 +0300

    add canvas state to redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import arrayReducer, { arrayAdapter } from './slices/arraySlice';
+import canvasReducer from './slices/canvasSlice';
 
 export const store = configureStore({
     reducer: {
         array: arrayReducer,
+        canvas: canvasReducer,
     },
 });
 
@@ -13,3 +16,7 @@ export type AppDispatch = typeof store.dispatch;
 export const arraySelectors = arrayAdapter.getSelectors(
     (state: RootState) => state.array
 );
+
+type DispatchFunc = () => AppDispatch;
+export const useAppDispatch: DispatchFunc = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 export type { default as Bar } from './Bar';
 export type { default as ColorRange } from './ColorRange';
+export { default as DSAType } from './DSAType';
 export type { default as Modal } from './Modal';
 export type { default as Size } from './Size';
