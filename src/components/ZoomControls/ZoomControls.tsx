cc9e83e6397d0408f99f404ef35343c2aa381f9f
kalibetre <kalbetre@icloud.com>
2023-02-20 15:23:45 +0300

    add ZoomControls Component

---
+++ src/components/ZoomControls/ZoomControls.tsx
@@ -0,0 +1,38 @@
+import { ZoomExtentsIcon, ZoomInIcon, ZoomOutIcon } from '../../icons';
+import styles from './ZoomControls.module.css';
+
+interface ZoomControlsProps {
+    handleZoomIn: () => void;
+    handleZoomOut: () => void;
+    handleZoomExtents: () => void;
+}
+
+const ZoomControls = (props: ZoomControlsProps) => {
+    return (
+        <div className={styles.zoomControls}>
+            <button className={styles.zoomBtn} onClick={props.handleZoomIn}>
+                <span>
+                    <ZoomInIcon />
+                </span>
+                Zoom In
+            </button>
+            <button className={styles.zoomBtn} onClick={props.handleZoomOut}>
+                <span>
+                    <ZoomOutIcon />
+                </span>
+                Zoom Out
+            </button>
+            <button
+                className={styles.zoomBtn}
+                onClick={props.handleZoomExtents}
+            >
+                <span>
+                    <ZoomExtentsIcon />
+                </span>
+                Extents
+            </button>
+        </div>
+    );
+};
+
+export default ZoomControls;
