19d789d66df009d659774c2e464b097138e64e92
kalibetre <kalbetre@icloud.com>
2023-02-20 16:53:17 +0300

    add convertToTree utility function

---
+++ src/utils/tree-converter.ts
@@ -0,0 +1,22 @@
+import { Node, TreeNode } from '../types';
+
+export const convertToTree = (
+    array: Node[],
+    i: number = 0
+): TreeNode | null => {
+    if (array.length === 0 || i >= array.length) return null;
+
+    const node: TreeNode = {
+        id: array[i].id,
+        value: array[i].value,
+        children: [],
+    };
+
+    const leftNode = convertToTree(array, 2 * i + 1);
+    if (leftNode) node.children[0] = leftNode;
+
+    const rightNode = leftNode ? convertToTree(array, 2 * i + 2) : null;
+    if (rightNode) node.children[1] = rightNode;
+
+    return node;
+};
