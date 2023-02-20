782e6e1dff3c5ad4a5df2caf3ad8f1f37e17c132
kalibetre <kalbetre@icloud.com>
2023-02-20 15:25:23 +0300

    allow switching between array and tree graphic

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,10 +1,13 @@
-import { ArrayGraphic } from '..';
+import { ArrayGraphic, TreeGraphic } from '..';
+import { useAppSelector } from '../../store/store';
+import { DSAType } from '../../types';
 import styles from './Canvas.module.css';
 
 const Canvas = () => {
+    const dsaType = useAppSelector((state) => state.canvas.dsaType);
     return (
         <div className={styles.container}>
-            <ArrayGraphic />
+            {dsaType === DSAType.ARRAY ? <ArrayGraphic /> : <TreeGraphic />}
         </div>
     );
 };
