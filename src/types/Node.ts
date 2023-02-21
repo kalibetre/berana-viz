2258923fd2ac0264c71d1bfca89e5f95867ba979
kalibetre <kalbetre@icloud.com>
2023-02-21 21:42:30 +0300

    add NodeStatus enum

--- src/types/Node.ts
+++ src/types/Node.ts
@@ -1,6 +1,13 @@
+export enum NodeStatus {
+    NORMAL,
+    VISITED,
+    ACTIVE,
+}
+
 interface Node {
     id: string;
     value: number;
+    status: NodeStatus;
 }
 
 export default Node;
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ export type { default as Bar } from './Bar';
 export type { default as ColorRange } from './ColorRange';
 export { default as DSAType } from './DSAType';
 export type { default as Modal } from './Modal';
+export { NodeStatus } from './Node';
 export type { default as Node } from './Node';
 export type { default as Size } from './Size';
 export { default as SortingAlgo } from './SortingAlgo';
