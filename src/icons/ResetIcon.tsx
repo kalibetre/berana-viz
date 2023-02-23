df85bd582a18988f9488f849249d8f0e97731cb5
kalibetre <kalbetre@icloud.com>
2023-02-23 23:31:51 +0300

    add reset icon

---
+++ src/icons/ResetIcon.tsx
@@ -0,0 +1,11 @@
+const ResetIcon = () => {
+    return (
+        <img
+            style={{ width: '100%', height: '100%' }}
+            src="/images/reset-icon.svg"
+            alt="Document Icon"
+        />
+    );
+};
+
+export default ResetIcon;
