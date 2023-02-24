49adac20849c96da0583aaf39f6a3d9b5e1a45b1
kalibetre <kalbetre@icloud.com>
2023-02-24 16:41:03 +0300

    add auth components

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,17 +1,48 @@
+import { ReactNode, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { firebaseAuth } from '../../auth/firebase';
 import styles from './Auth.module.css';
 
-export const Auth = () => {
+interface AuthProps {
+    children: ReactNode;
+    title: string;
+}
+
+const Auth = (props: AuthProps) => {
+    const navigate = useNavigate();
+    const [checkingUser, setCheckingUser] = useState<boolean>(true);
+
+    useEffect(() => {
+        firebaseAuth.onAuthStateChanged((user) => {
+            setCheckingUser(false);
+            if (user) navigate('/', { replace: true });
+        });
+    }, [navigate]);
+
     return (
-        <div className={styles.modalContainer}>
-            <header>BeranaViz</header>
-            <form>
-                <div className={styles.inputRow}>
-                    <div className={styles.inputContainer}>
-                        <label htmlFor="" className={styles.inputLabel}></label>
-                        <input type="text" className={styles.input} />
+        <div className={styles.container}>
+            <div className={styles.formContainer}>
+                <header>
+                    <img
+                        className={styles.logo}
+                        src="/images/logo.png"
+                        alt="BeranaVis Logo"
+                    />
+                    <div className={styles.logoTitles}>
+                        <h1 className={styles.logoTitle}>BeranaVis</h1>
+                        <h2 className={styles.logoSubTitle}>{props.title}</h2>
                     </div>
-                </div>
-            </form>
+                </header>
+                {checkingUser ? (
+                    <div className={styles.loading}>
+                        Please wait, checking ....
+                    </div>
+                ) : (
+                    props.children
+                )}
+            </div>
         </div>
     );
 };
+
+export default Auth;
---
+++ src/components/Auth/SignInForm.tsx
@@ -0,0 +1,108 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { signIn } from '../../auth/auth';
+import { firebaseAuth } from '../../auth/firebase';
+import Auth from './Auth';
+import styles from './Auth.module.css';
+
+interface SinInInput {
+    email: string;
+    password: string;
+}
+
+const SignIn = () => {
+    const navigate = useNavigate();
+    const [formError, setFormError] = useState<string>('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<SinInInput>();
+
+    const handelSignIn = (data: any) => {
+        setIsLoading(true);
+        setFormError('');
+        signIn(data.email, data.password)
+            .then(() => {
+                if (firebaseAuth.currentUser) navigate('/', { replace: true });
+            })
+            .catch(() => {
+                setFormError(
+                    'Invalid email address or password. Please try again.'
+                );
+                setIsLoading(false);
+            });
+    };
+
+    return (
+        <Auth title="Sign In">
+            <form onSubmit={handleSubmit(handelSignIn)}>
+                {formError.length > 0 && (
+                    <div className={styles.formError}>
+                        <span>{formError}</span>
+                    </div>
+                )}
+                <div className={styles.inputRow}>
+                    <div className={styles.inputContainer}>
+                        <label htmlFor="email" className={styles.inputLabel}>
+                            Email
+                        </label>
+                        <input
+                            id="email"
+                            type="email"
+                            className={styles.input}
+                            {...register('email', {
+                                required: 'Email Required',
+                            })}
+                        />
+                        <div className={styles.errorContainer}>
+                            <span>{errors.email?.message}</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.inputRow}>
+                    <div className={styles.inputContainer}>
+                        <label htmlFor="password" className={styles.inputLabel}>
+                            Password
+                        </label>
+                        <input
+                            id="password"
+                            type="password"
+                            className={styles.input}
+                            {...register('password', {
+                                required: 'Password Required',
+                                min: {
+                                    value: 6,
+                                    message:
+                                        'should be at least 6 characters long',
+                                },
+                            })}
+                        />
+                        <div className={styles.errorContainer}>
+                            <span>{errors.password?.message}</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.optionContainer}>
+                    <span>Need account?</span>
+                    <Link to="/auth/signup" className={styles.switchSpan}>
+                        Sign Up
+                    </Link>
+                </div>
+                <div className={styles.buttonRow}>
+                    <input
+                        type="submit"
+                        value="Sign In"
+                        className={styles.primaryBtn}
+                        disabled={isLoading}
+                    />
+                </div>
+            </form>
+        </Auth>
+    );
+};
+
+export default SignIn;
